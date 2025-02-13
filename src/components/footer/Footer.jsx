0ca226c5e5de1c3828919b37ef45b93a236796e2
be <be@gmail.com>
2025-02-13 18:15:23 +0500

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://github.com/behruzbektester/month6leasson2">
+          Behruzbek Ibrohimov
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
